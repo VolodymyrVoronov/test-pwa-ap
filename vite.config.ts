85de5d28a842baabc8845411c1cfbe7bd701aab9
Volodymyr Voronov <malones.vladimir@gmail.com>
2026-03-27 15:06:16 +0100

    feat: add PWA support with vite-plugin-pwa and update package.json - Added vite-plugin-pwa to package.json dependencies - Configured PWA settings in vite.config.ts including manifest and workbox options - Minor formatting change in main.tsx for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,59 @@ import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Audio App",
+        short_name: "AudioApp",
+        description: "Audio player / DAW",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "/",
+
+        // icons: [
+        //   {
+        //     src: "/pwa-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        //   },
+        //   {
+        //     src: "/pwa-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //   },
+        //   {
+        //     src: "/pwa-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        // ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,mp3,wav}"],
+      },
+
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
-
