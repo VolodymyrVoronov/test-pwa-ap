f2836a58661203622aeb37822cca4bbaf3f3ced6
Volodymyr Voronov <malones.vladimir@gmail.com>
2026-03-27 15:12:34 +0100

    feat: update PWA icons in vite.config.ts for improved app branding

--- vite.config.ts
+++ vite.config.ts
@@ -24,24 +24,24 @@ export default defineConfig({
         display: "standalone",
         start_url: "/",
 
-        // icons: [
-        //   {
-        //     src: "/pwa-192x192.png",
-        //     sizes: "192x192",
-        //     type: "image/png",
-        //   },
-        //   {
-        //     src: "/pwa-512x512.png",
-        //     sizes: "512x512",
-        //     type: "image/png",
-        //   },
-        //   {
-        //     src: "/pwa-512x512.png",
-        //     sizes: "512x512",
-        //     type: "image/png",
-        //     purpose: "any maskable",
-        //   },
-        // ],
+        icons: [
+          {
+            src: "/favicon.svg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicon.svg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/favicon.svg",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
 
       workbox: {
